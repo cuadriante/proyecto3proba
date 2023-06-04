41f2489bd1af4c334d424c3b4bd137fcaf4cb9cd
adri <62965634+cuadriante@users.noreply.github.com>
2023-06-03 22:14:40 -0600

    t test

--- main.py
+++ main.py
@@ -1,16 +1,74 @@
-# This is a sample Python script.
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# se establecen las hipotesis:
+#       h0 (nula): miu=70
+#       h1 (alternativa): miu>=70
+
+# recopilación de datos durante 90 días:
+
+ruta = 'Conjunto_datos_proyecto3.xlsx'
+datos = pd.read_excel(ruta)
+rendimiento_inicial = datos['Inicial'].to_numpy()
+rendimiento_primer_cambio = datos['Primer_cambio'].to_numpy()
+rendimiento_segundo_cambio = datos['Segundo_cambio'].to_numpy()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+### --- COMPROBACION --- ###
+# funcion: ttest_1samp: prueba de hipotesis t de una muestra, compara la media
+#  con la hipotesis nula y determina si es significativamente diferente al valor de referencia
+# entrada: muestra, valor de referencia
+# salida: estadistica de prueba, valor p
+
+def t_test(rendimiento):
+        media_muestral = np.mean(rendimiento)
+        desviacion_estandar_muestral = np.std(rendimiento, ddof=1)  # ddof=1 para utilizar la fórmula de la desviación estándar muestral
+        valor_referencia = 0.7 * media_muestral
+        t_stat, p_value = stats.ttest_1samp(rendimiento, valor_referencia)
+        print(p_value)
+
+### INICIAL ###
+print("--------------------------------Inicial--------------------------------")
+print(rendimiento_inicial)
+t_test(rendimiento_inicial)
+
+### PRIMER CAMBIO ###
+print("-----------------------------Primer Cambio-----------------------------")
+print(rendimiento_primer_cambio)
+t_test(rendimiento_primer_cambio)
+
+### SEGUNDO CAMBIO ###
+print("----------------------------Segundo Cambio-----------------------------")
+print(rendimiento_segundo_cambio)
+t_test(rendimiento_segundo_cambio)
+
+### --- COMPARACION --- ###
+
+# funcion: ttest_ind: hace una prueba de hipotesis t entre dos muestras independientes
+# y deteermina si las medias son significativamente diferentes
+# entrada: muestra1, muestra2
+# salida: estadistica de prueba (diferencia entre la media y el valor de referencia) y valor p
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# INICIAL VS PRIMER CAMBIO
+print("--------------------------------I VS P--------------------------------")
+t_stat_ip, valor_p_ip = stats.ttest_ind(rendimiento_inicial, rendimiento_primer_cambio)
+print(valor_p_ip)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# PRIMER CAMBIO VS SEGUNDO CAMBIO
+print("--------------------------------P VS S--------------------------------")
+t_stat_ps, valor_p_ps = stats.ttest_ind(rendimiento_primer_cambio, rendimiento_segundo_cambio)
+print(valor_p_ps)
+# INICIAL VS SEGUNDO CAMBIO
+print("--------------------------------I VS S--------------------------------")
+t_stat_is, valor_p_is = stats.ttest_ind(rendimiento_inicial, rendimiento_segundo_cambio)
+print(valor_p_is)
+
+
+
+
+
+
+
