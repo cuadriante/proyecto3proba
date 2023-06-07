4befdfc59aee522f7dcbd3b671dd6cc71154ecd3
adri <62965634+cuadriante@users.noreply.github.com>
2023-06-06 22:55:04 -0600

    2.2 analitico

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import math
 
+################## 2.1 ##################
 # se establecen las hipotesis:
 #       h0 (nula): miu=70
 #       h1 (alternativa): miu>=70
@@ -24,11 +25,13 @@ rendimiento_segundo_cambio = datos['Segundo_cambio'].to_numpy()
 # salida: estadistica de prueba, valor p
 
 def t_test(rendimiento):
-        media_muestral = np.mean(rendimiento)
-        desviacion_estandar_muestral = np.std(rendimiento, ddof=1)  # ddof=1 para utilizar la fórmula de la desviación estándar muestral
-        # a: rendimiennto, popmean: miu h0, nan_policy: omit, alternative: greater
-        t_stat, p_value = stats.ttest_1samp(a=rendimiento, popmean=70, nan_policy='omit', alternative='greater')
-        print(p_value)
+    media_muestral = np.mean(rendimiento)
+    desviacion_estandar_muestral = np.std(rendimiento,
+                                          ddof=1)  # ddof=1 para utilizar la fórmula de la desviación estándar muestral
+    # a: rendimiennto, popmean: miu h0, nan_policy: omit, alternative: greater
+    t_stat, p_value = stats.ttest_1samp(a=rendimiento, popmean=70, nan_policy='omit', alternative='greater')
+    print(p_value)
+
 
 ### INICIAL ###
 print("--------------------------------Inicial--------------------------------")
@@ -55,28 +58,31 @@ t_test(rendimiento_segundo_cambio)
 
 # INICIAL VS PRIMER CAMBIO
 print("--------------------------------P VS I--------------------------------")
-t_stat_ip, valor_p_ip = stats.ttest_ind(a=rendimiento_primer_cambio, b=rendimiento_inicial, nan_policy='omit', alternative='greater')
+t_stat_ip, valor_p_ip = stats.ttest_ind(a=rendimiento_primer_cambio, b=rendimiento_inicial, nan_policy='omit',
+                                        alternative='greater')
 print(valor_p_ip)
 
 # INICIAL VS SEGUNDO CAMBIO
 print("--------------------------------S VS I--------------------------------")
-t_stat_is, valor_p_is = stats.ttest_ind(a=rendimiento_segundo_cambio, b=rendimiento_inicial, nan_policy='omit', alternative='greater')
+t_stat_is, valor_p_is = stats.ttest_ind(a=rendimiento_segundo_cambio, b=rendimiento_inicial, nan_policy='omit',
+                                        alternative='greater')
 print(valor_p_is)
 
 # PRIMER CAMBIO VS SEGUNDO CAMBIO
 print("--------------------------------S VS P--------------------------------")
-t_stat_ps, valor_p_ps = stats.ttest_ind(a=rendimiento_segundo_cambio, b=rendimiento_primer_cambio, nan_policy='omit', alternative='greater')
+t_stat_ps, valor_p_ps = stats.ttest_ind(a=rendimiento_segundo_cambio, b=rendimiento_primer_cambio, nan_policy='omit',
+                                        alternative='greater')
 print(valor_p_ps)
 
+################## 2.2 ##################
 
-
-# METODO MAXIMA VEROSIMILITUD
+### --- METODO MAXIMA VEROSIMILITUD --- ###
 # metodo analitico
 # entrada: (array) rendimiento
 # salida: desviación estándar
 def mle_desvesta(rendimiento):
     sumatoria = 0
-    promedio = stats.mean(rendimiento)
+    promedio = np.mean(rendimiento)
     for xi in rendimiento:
         sumatoria = (xi - promedio) ** 2
     varianza = (1 / len(rendimiento)) * sumatoria
@@ -84,3 +90,20 @@ def mle_desvesta(rendimiento):
 
 # metodo empírico
 
+### INICIAL ###
+print("----------------------Desviaciones Inicial----------------------")
+print(mle_desvesta(rendimiento_inicial))
+_, std_inicial = stats.norm.fit(rendimiento_inicial)
+print(std_inicial)
+
+### PRIMER CAMBIO ###
+print("-------------------Desviaciones Primer Cambio-------------------")
+print(mle_desvesta(rendimiento_primer_cambio))
+_, std_primer = stats.norm.fit(rendimiento_primer_cambio)
+print(std_primer)
+
+### SEGUNDO CAMBIO ###
+print("------------------Desviaciones Segundo Cambio-------------------")
+print(mle_desvesta(rendimiento_segundo_cambio))
+_, std_segundo = stats.norm.fit(rendimiento_segundo_cambio)
+print(std_segundo)
